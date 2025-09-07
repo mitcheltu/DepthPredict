635884521d0a46d50dbb37fbce0cb3ccc7de4624
mitcheltu <tumk@uci.edu>
2025-09-06 20:48:26 -0700

    Changed base vite config

--- depth-predict-app/frontend/depth-predict/vite.config.js
+++ depth-predict-app/frontend/depth-predict/vite.config.js
@@ -9,5 +9,7 @@ export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
   mimeTypes: {
       'application/wasm': ['wasm']
-    }
+    },
+    base: '/DepthPredict/',
+
 })
